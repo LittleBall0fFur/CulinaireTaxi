736695840b443f889d1208595362beaaed7992d5
shcalbers <34623185+shcalbers@users.noreply.github.com>
2019-03-27 22:37:21 +0100

    Add empty Login and Register Pages

---
+++ CulinaireTaxi/Pages/Login.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using CulinaireTaxi.Authentication;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CulinaireTaxi.Pages
+{
+
+    public class LoginModel : PageModel
+    {
+
+	public UserAgent UserAgent
+	{
+	    get;
+	    private set;
+	}
+
+	public LoginModel(UserAgent userAgent)
+	{
+	    UserAgent = userAgent;
+	}
+
+    }
+
+}
