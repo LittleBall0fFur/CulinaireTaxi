670d2cee0753d2d375d1585c72029f441b1dee85
shcalbers <34623185+shcalbers@users.noreply.github.com>
2019-03-29 23:35:52 +0100

    Change 'Database/CulinaireTaxiDB.cs'
    Changes the 'Initialize' method to make sure it properly disposes its resources.

--- CulinaireTaxi/Database/CulinaireTaxiDB.cs
+++ CulinaireTaxi/Database/CulinaireTaxiDB.cs
@@ -7,100 +7,108 @@ namespace CulinaireTaxi.Database
     public static class CulinaireTaxiDB
     {
 
-	private static MySqlConnection m_connection;
+	private static readonly string CONNECTION_STRING = /*"server=localhost;uid=root;pwd=rootpass;database=culinairetaxi;"*/"server=sql7.freemysqlhosting.net;uid=sql7285675;pwd=ITnJKJJc4r;database=sql7285675";
 
 	static CulinaireTaxiDB()
 	{
-	    m_connection = new MySqlConnection(/*"server=localhost;uid=root;pwd=rootpass;database=culinairetaxi;"*/"server=sql7.freemysqlhosting.net;uid=sql7285675;pwd=ITnJKJJc4r;database=sql7285675");
-
 	    Initialize();
 	}
 
 	private static void Initialize()
 	{
-	    m_connection.Open();
+	    using (var connection = new MySqlConnection(CONNECTION_STRING))
+	    {
+		connection.Open();
 
-	    #region CREATE_TABLE_COMMANDS
+		using (var CREATE_ACCOUNT_TABLE	    = connection.CreateCommand())
+		using (var CREATE_CUSTOMER_TABLE    = connection.CreateCommand())
+		using (var CREATE_COMPANY_TABLE	    = connection.CreateCommand())
+		using (var CREATE_RATING_TABLE	    = connection.CreateCommand())
+		using (var CREATE_RESERVATION_TABLE = connection.CreateCommand())
+		{
+		    #region CREATE_TABLE_COMMANDS
 
-	    var CREATE_ACCOUNT_TABLE = new MySqlCommand(
-		"CREATE TABLE IF NOT EXISTS Account" +
-		"(id BIGINT NOT NULL AUTO_INCREMENT," +
-		" type TINYINT NOT NULL," +
-		" email VARCHAR(320) NOT NULL UNIQUE," +
-		" password VARCHAR(128) NOT NULL," +
-		" county VARCHAR(16)," +
-		" city VARCHAR(48)," +
-		" street VARCHAR(96)," +
-		" postal_code VARCHAR(32)," +
-		" phone_number VARCHAR(32)," +
-		" PRIMARY KEY (id))", m_connection);
+		    CREATE_ACCOUNT_TABLE.CommandText = 
+		    "CREATE TABLE IF NOT EXISTS Account" +
+		    "(id BIGINT NOT NULL AUTO_INCREMENT," +
+		    " type TINYINT NOT NULL," +
+		    " email VARCHAR(320) NOT NULL UNIQUE," +
+		    " password VARCHAR(128) NOT NULL," +
+		    " county VARCHAR(16)," +
+		    " city VARCHAR(48)," +
+		    " street VARCHAR(96)," +
+		    " postal_code VARCHAR(32)," +
+		    " phone_number VARCHAR(32)," +
+		    " PRIMARY KEY (id))";
 
-	    var CREATE_CUSTOMER_TABLE = new MySqlCommand(
-		"CREATE TABLE IF NOT EXISTS Customer" +
-		"(account_id BIGINT NOT NULL," +
-		" first_name TEXT NOT NULL," +
-		" last_name TEXT NOT NULL," +
-		" PRIMARY KEY (account_id)," +
-		" FOREIGN KEY (account_id)" +
-		" REFERENCES Account(id)" +
-		" ON DELETE CASCADE" +
-		" ON UPDATE CASCADE)", m_connection);
+		    CREATE_CUSTOMER_TABLE.CommandText =
+		    "CREATE TABLE IF NOT EXISTS Customer" +
+		    "(account_id BIGINT NOT NULL," +
+		    " first_name TEXT NOT NULL," +
+		    " last_name TEXT NOT NULL," +
+		    " PRIMARY KEY (account_id)," +
+		    " FOREIGN KEY (account_id)" +
+		    " REFERENCES Account(id)" +
+		    " ON DELETE CASCADE" +
+		    " ON UPDATE CASCADE)";
 
-	    var CREATE_COMPANY_TABLE = new MySqlCommand(
-		"CREATE TABLE IF NOT EXISTS Company" +
-		"(account_id BIGINT NOT NULL," +
-		" type TINYINT NOT NULL," +
-		" name TEXT NOT NULL," +
-		" description TEXT NOT NULL," +
-		" PRIMARY KEY (account_id)," +
-		" FOREIGN KEY (account_id)" +
-		" REFERENCES Account(id)" +
-		" ON DELETE CASCADE" +
-		" ON UPDATE CASCADE)", m_connection);
+		    CREATE_COMPANY_TABLE.CommandText =
+		    "CREATE TABLE IF NOT EXISTS Company" +
+		    "(account_id BIGINT NOT NULL," +
+		    " type TINYINT NOT NULL," +
+		    " name TEXT NOT NULL," +
+		    " description TEXT NOT NULL," +
+		    " PRIMARY KEY (account_id)," +
+		    " FOREIGN KEY (account_id)" +
+		    " REFERENCES Account(id)" +
+		    " ON DELETE CASCADE" +
+		    " ON UPDATE CASCADE)";
 
-	    var CREATE_RATING_TABLE = new MySqlCommand(
-		"CREATE TABLE IF NOT EXISTS gives_rating" +
-		"(customer_id BIGINT NOT NULL," +
-		" company_id BIGINT NOT NULL," +
-		" rating TINYINT NOT NULL," +
-		" PRIMARY KEY (customer_id, company_id)," +
-		" FOREIGN KEY (customer_id)" +
-		" REFERENCES Customer(account_id)" +
-		" ON DELETE CASCADE" +
-		" ON UPDATE CASCADE," +
-		" FOREIGN KEY (company_id)" +
-		" REFERENCES Company(account_id)" +
-		" ON DELETE CASCADE" +
-		" ON UPDATE CASCADE)", m_connection);
+		    CREATE_RATING_TABLE.CommandText =
+		    "CREATE TABLE IF NOT EXISTS gives_rating" +
+		    "(customer_id BIGINT NOT NULL," +
+		    " company_id BIGINT NOT NULL," +
+		    " rating TINYINT NOT NULL," +
+		    " PRIMARY KEY (customer_id, company_id)," +
+		    " FOREIGN KEY (customer_id)" +
+		    " REFERENCES Customer(account_id)" +
+		    " ON DELETE CASCADE" +
+		    " ON UPDATE CASCADE," +
+		    " FOREIGN KEY (company_id)" +
+		    " REFERENCES Company(account_id)" +
+		    " ON DELETE CASCADE" +
+		    " ON UPDATE CASCADE)";
 
-	    var CREATE_RESERVATION_TABLE = new MySqlCommand(
-		"CREATE TABLE IF NOT EXISTS Reservation" +
-		"(id BIGINT NOT NULL AUTO_INCREMENT," +
-		" customer_id BIGINT NOT NULL," +
-		" company_id BIGINT NOT NULL," +
-		" from_date DATETIME NOT NULL," +
-		" till_date DATETIME NOT NULL," +
-		" guests_amount INT NOT NULL," +
-		" status TINYINT NOT NULL," +
-		" PRIMARY KEY (id)," +
-		" FOREIGN KEY (customer_id)" +
-		" REFERENCES Customer(account_id)" +
-		" ON DELETE CASCADE" +
-		" ON UPDATE CASCADE," +
-		" FOREIGN KEY (company_id)" +
-		" REFERENCES Company(account_id)" +
-		" ON DELETE CASCADE" +
-		" ON UPDATE CASCADE)", m_connection);
+		    CREATE_RESERVATION_TABLE.CommandText =
+		    "CREATE TABLE IF NOT EXISTS Reservation" +
+		    "(id BIGINT NOT NULL AUTO_INCREMENT," +
+		    " customer_id BIGINT NOT NULL," +
+		    " company_id BIGINT NOT NULL," +
+		    " from_date DATETIME NOT NULL," +
+		    " till_date DATETIME NOT NULL," +
+		    " guests_amount INT NOT NULL," +
+		    " status TINYINT NOT NULL," +
+		    " PRIMARY KEY (id)," +
+		    " FOREIGN KEY (customer_id)" +
+		    " REFERENCES Customer(account_id)" +
+		    " ON DELETE CASCADE" +
+		    " ON UPDATE CASCADE," +
+		    " FOREIGN KEY (company_id)" +
+		    " REFERENCES Company(account_id)" +
+		    " ON DELETE CASCADE" +
+		    " ON UPDATE CASCADE)";
 
-	    #endregion
+		    #endregion
 
-	    CREATE_ACCOUNT_TABLE.ExecuteNonQuery();
-	    CREATE_CUSTOMER_TABLE.ExecuteNonQuery();
-	    CREATE_COMPANY_TABLE.ExecuteNonQuery();
-	    CREATE_RATING_TABLE.ExecuteNonQuery();
-	    CREATE_RESERVATION_TABLE.ExecuteNonQuery();
+		    CREATE_ACCOUNT_TABLE.ExecuteNonQuery();
+		    CREATE_CUSTOMER_TABLE.ExecuteNonQuery();
+		    CREATE_COMPANY_TABLE.ExecuteNonQuery();
+		    CREATE_RATING_TABLE.ExecuteNonQuery();
+		    CREATE_RESERVATION_TABLE.ExecuteNonQuery();
+		}
 
-	    m_connection.Close();
+		connection.Close();
+	    }
 	}
 
 	public static Account RetrieveAccount()
