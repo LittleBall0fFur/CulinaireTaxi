2de9ec7412736c10de0b4aec253be15c8cc1200c
shcalbers <34623185+shcalbers@users.noreply.github.com>
2019-05-21 21:45:26 +0200

    Add Admin AccountType

--- CulinaireTaxi/Database/Entities/Account.cs
+++ CulinaireTaxi/Database/Entities/Account.cs
@@ -4,54 +4,55 @@ namespace CulinaireTaxi.Database.Entities
 
     public enum AccountType : byte
     {
-	CUSTOMER = 0,
-	RESTAURANT = 1,
-	TAXI = 2
+        CUSTOMER = 0,
+        RESTAURANT = 1,
+        TAXI = 2,
+        ADMIN = 3
     }
 
     public class Account
     {
 
-	public long Id
-	{
-	    get;
-	    set;
-	}
+        public long Id
+        {
+            get;
+            set;
+        }
 
-	public AccountType AccountType
-	{
-	    get;
-	    set;
-	}
+        public AccountType AccountType
+        {
+            get;
+            set;
+        }
 
-	public string Email
-	{
-	    get;
-	    set;
-	}
+        public string Email
+        {
+            get;
+            set;
+        }
 
-	public string Password
-	{
-	    get;
-	    set;
-	}
+        public string Password
+        {
+            get;
+            set;
+        }
 
-	public ContactDetails Contact
-	{
-	    get;
-	    set;
-	}
+        public ContactDetails Contact
+        {
+            get;
+            set;
+        }
 
-	public long? CompanyId
-	{
-	    get;
-	    set;
-	}
+        public long? CompanyId
+        {
+            get;
+            set;
+        }
 
-	public Account()
-	{
-	    Contact = new ContactDetails();
-	}
+        public Account()
+        {
+            Contact = new ContactDetails();
+        }
 
     }
 
