b5068bd2e727869b8b84d74389e4c794104d8eb9
Naomi Poort <naomi.roberta.ans.poort@gmail.com>
2019-06-05 15:44:41 +0200

    Functionality customerpage updated and fixt

--- CulinaireTaxi/Pages/App/CustomerPage.cshtml.cs
+++ CulinaireTaxi/Pages/App/CustomerPage.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CulinaireTaxi.Authentication;
 using CulinaireTaxi.Database;
+using CulinaireTaxi.Database.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,105 +13,157 @@ namespace CulinaireTaxi.Pages
 {
     public class CustomerPageModel : PageModel
     {
-        private UserAgent _user;
-        public CustomerPageModel(UserAgent user) {
-            _user = user;
-        }
+        public const string POSTID_CALENDAR_BACKWARDS = "sub_date";
+        public const string POSTID_CALENDAR_TODAY = "today_date";
+        public const string POSTID_CALENDAR_FORWARDS = "add_date";
 
-        [BindProperty, Required]
-        public string form_id
-        {
-            get;
-            set;
-        }
+        public const string POSTID_CREATE_RESERVATION = "create_reservation";
+        public const string POSTID_DELETE_RESERVATION = "delete_reservation";
+        public const string POSTID_UPDATE_INFO = "update_info";
+
+        List<Reservation> reservations = new List<Reservation>();
+
+        private readonly DateTime TODAY = DateTime.Today;
+
+        [BindProperty]
+        [Required]
+        public string PostID { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public UserAgent UserAgent { get; set; }
+
+        public long ResID{ get; set; }
+
+        [BindProperty]
+        public string form_id { get; set; }
 
         // user detail form
         [BindProperty]
-        public string county
-        {
-            get;
-            set;
-        }
+        public string country { get; set; }
+
         [BindProperty]
-        public string city
-        {
-            get;
-            set;
-        }
+        public string city { get; set; }
+
         [BindProperty]
-        public string streetname
-        {
-            get;
-            set;
-        }
+        public string streetname { get; set; }
+
         [BindProperty]
-        public string postalcode
-        {
-            get;
-            set;
-        }
+        public string postalcode { get; set; }
+
         [BindProperty]
-        public string phonenumber
-        {
-            get;
-            set;
-        }
+        public string phonenumber { get; set; }
 
         // reseveer
-        [BindProperty, Required]
-        public long restaurant
+        [BindProperty]
+        public long restaurant { get; set; }
+
+        [BindProperty]
+        public string fromdate { get; set; }
+
+        [BindProperty]
+        public string fromtime { get; set; }
+
+        [BindProperty]
+        public string tilldate { get; set; }
+
+        [BindProperty]
+        public string tilltime { get; set; }
+
+        [BindProperty]
+        public int guestsamount { get; set; }
+
+        public CustomerPageModel(UserAgent userAgent)
         {
-            get;
-            set;
-        }
-        [BindProperty, Required]
-        public string fromdate
-        {
-            get;
-            set;
-        }
-        [BindProperty, Required]
-        public string fromtime
-        {
-            get;
-            set;
-        }
-        [BindProperty, Required]
-        public string tilldate
-        {
-            get;
-            set;
-        }
-        [BindProperty, Required]
-        public string tilltime
-        {
-            get;
-            set;
-        }
-        [BindProperty, Required]
-        public int guestsamount
-        {
-            get;
-            set;
+            UserAgent = userAgent;
+            Date = DateTime.Today;
         }
 
         public void OnPost()
         {
-            if (!_user.IsAuthenticated) {
+            if (!ModelState.IsValid)
+            {
                 return;
             }
 
-            if (form_id == "0") {
-                Database.Entities.ContactDetails contact = _user.Account.Contact;
-                contact.County = county;
-                contact.City = city;
-                contact.Street = streetname;
-                contact.PostalCode = postalcode;
-                contact.PhoneNumber = phonenumber;
-                AccountTable.UpdateAccountContactDetails(_user.Account.Id, contact);
-            } else if (form_id == "1" && ModelState.IsValid) {
-                ReservationTable.CreateReservation(_user.Account.Id, restaurant, DateTime.Parse(fromdate + " " + fromtime), DateTime.Parse(tilldate + " " + tilltime), guestsamount);
+            switch (PostID)
+            {
+                case POSTID_CALENDAR_BACKWARDS:
+                    POST_CalendarBackwards();
+                    break;
+                case POSTID_CALENDAR_TODAY:
+                    POST_CalendarToday();
+                    break;
+                case POSTID_CALENDAR_FORWARDS:
+                    POST_CalendarForwards();
+                    break;
+                case POSTID_CREATE_RESERVATION:
+                    POST_Create_Reservation();
+                    break;
+                case POSTID_DELETE_RESERVATION:
+                    POST_Delete_Reservation();
+                    break;
+                case POSTID_UPDATE_INFO:
+                    POST_Update_Info();
+                    break;
             }
+        }
+
+        public DateTime GetDate()
+        {
+            return Date;
+        }
+
+        public void SetDate(DateTime _date)
+        {
+            Date = _date;
+        }
+
+        private void POST_CalendarBackwards()
+        {
+            Date = Date.AddDays(-1);
+        }
+
+        private void POST_CalendarForwards()
+        {
+            Date = Date.AddDays(1);
+        }
+
+        private void POST_CalendarToday()
+        {
+            Date = TODAY;
+        }
+
+        private void POST_Create_Reservation()
+        {
+            //TO-DO fix validation for input fields
+            if (!UserAgent.IsAuthenticated)
+            {
+                return;
+            }
+            ReservationTable.CreateReservation(UserAgent.Account.Id, restaurant, DateTime.Parse(fromdate + " " + fromtime), DateTime.Parse(tilldate + " " + tilltime), guestsamount);
+        }
+
+        private void POST_Update_Info()
+        {
+            Database.Entities.ContactDetails contact = UserAgent.Account.Contact;
+            contact.County = country;
+            contact.City = city;
+            contact.Street = streetname;
+            contact.PostalCode = postalcode;
+            contact.PhoneNumber = phonenumber;
+            AccountTable.UpdateAccountContactDetails(UserAgent.Account.Id, contact);
+        }
+
+        public void AddReservation(Reservation res)
+        {
+            resID = reservations.Count();
+            reservations.Add(res);
+        }
+
+        private void POST_Delete_Reservation()
+        {
+            ReservationTable.DeleteReservation(long.Parse(Request.Form["ResID"]));
         }
     }
 }
