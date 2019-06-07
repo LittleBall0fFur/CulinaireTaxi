c9a6287ba7f1d7ca5bb9a16e0668efead8f7b768
shcalbers <34623185+shcalbers@users.noreply.github.com>
2019-06-07 17:52:09 +0200

    Create separate pages for Restaurants and Taxi's
    Splits 'CompanyPage.cshtml' into two pages: 'Restaurant.cshtml' and
    'TaxiCompany.cshtml'.

---
+++ CulinaireTaxi/wwwroot/js/modal.js
@@ -0,0 +1,31 @@
+﻿
+var current_modal = null;
+
+function initializeModal(modal) {
+    //Ensure modal is hidden.
+    modal.style.display = "none";
+
+    var closeModal_button = modal.getElementsByClassName("modal-close")[0];
+    closeModal_button.onclick = function () { closeModal(); };
+
+    //Close the modal when the user clicks outside of it.
+    window.onclick = function (event) {
+        if (event.target == current_modal) {
+            closeModal();
+        }
+    };
+}
+
+function openModal(modal) {
+    if (current_modal != null) {
+        return;
+    }
+
+    current_modal = modal;
+    current_modal.style.display = "block";
+}
+
+function closeModal() {
+    current_modal.style.display = "none";
+    current_modal = null;
+}
