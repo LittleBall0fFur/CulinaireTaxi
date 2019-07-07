ee25a2d86c65fa2efac97dba3237321c4e54c9d9
sidneydijkstra <sidney0099@hotmail.com>
2019-07-07 17:47:26 +0200

    added admin pannel to confirm company accounts

--- CulinaireTaxi/Pages/App/Admin.cshtml.cs
+++ CulinaireTaxi/Pages/App/Admin.cshtml.cs
@@ -1,21 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CulinaireTaxi.Pages.App
-{
-    public class AdminModel : PageModel
-    {
+namespace CulinaireTaxi.Pages.App{
+    public class AdminModel : PageModel{
         public const string POSTID_CONFIRM_COMPANY = "confirm_company";
         public const string POSTID_DECLINE_COMPANY = "decline_company";
+        
+        [BindProperty]
+        [Required]
+        public string postID { get; set; }
 
+        [BindProperty]
+        [Required]
+        public string companyID { get; set; }
 
-        public void OnGet()
-        {
+        [BindProperty]
+        [Required]
+        public string confirmType { get; set; }
 
+        public void OnPost(){
+            if (!ModelState.IsValid){
+                return;
+            }
+
+            if (postID == "CHECK_COMPANY") {
+                Database.CompanyTable.UpdateCompanyConfirm(long.Parse(companyID), confirmType == POSTID_CONFIRM_COMPANY);
+            }
         }
     }
 }
